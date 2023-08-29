78e4ac23bca141b5ff7797261db10f40f6c3b7e9
Kian Sharifi <kiansharifi88@gmail.com>
2023-08-28 21:21:55 -0700

    Writer and Reader Working

--- FileWriter.java
+++ FileWriter.java
@@ -1,17 +1,29 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
+
 public class FileWriter
 {
-    public static void main (String [] args)
+    public static String reader (String codeFile) throws IOException
     {
-        FileWriter file = new FileWriter ();
+        StringBuilder output = new StringBuilder ();
+        BufferedReader breader = new BufferedReader (new FileReader (codeFile));
+        while (breader.ready())
+        {
+            String s = breader.readLine ();
+            output.append(s);
+        }
+        breader.close();
+        return output.toString();
     }
 
-    this.array = new String [128];
-		
-    
+    public static void writer (String stuff, String codeFile) throws IOException
+    {
+        PrintWriter printer = new PrintWriter(codeFile);
+        printer.print (stuff);
+        printer.close ();
+    }
+
 }
---
+++ Tester.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Tester
+{
+    public static void main (String [] args) throws IOException
+    {
+        System.out.println (FileWriter.reader ("codeFile"));
+        FileWriter.writer ("pleasework", "codeFile");
+        System.out.println (FileWriter.reader ("codeFile"));
+
+    }
+}
