b32aff1d4e45e9d4e23acb50c2cbb4313be0d422
Kian Sharifi <kiansharifi88@gmail.com>
2023-08-28 21:18:25 -0700

    Writer and Reader

--- FileWriter.java
+++ FileWriter.java
@@ -1,7 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 public class FileWriter
 {
     public static void main (String [] args)
     {
-
+        FileWriter file = new FileWriter ();
     }
+
+    this.array = new String [128];
+		
+    
 }
